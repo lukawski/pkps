6cdf94dab00f58c69ffb1830f9e32e2c678089da
wdddy <ka.lukawski@gmail.com>
2016-09-30 21:11:51 +0000

    added footer block

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
     $("a").on('click', function(event){
-        console.log(this.hash);
-        if(this.hash !== " "){
+        if(this.href.split('#')[0] == window.location.href.split('#')[0]  && this.hash !== " "){
             event.preventDefault();
-            
             $('body, html').animate({
-                scrollTop: $(this.hash).offset().top - $('#navbar').height()
+                scrollTop: $(this.hash).offset().top - $('#navbarFixed').height()
             }, 800);
         }
     });
