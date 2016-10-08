2ed65e989e6dc438aee6239327616f7f356a9c72
wdddy <ka.lukawski@gmail.com>
2016-10-08 22:45:53 +0000

    chaning hash in url

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
     $("a").on('click', function(event){
         if(this.href.split('#')[0] == window.location.href.split('#')[0]  && this.hash !== " "){
             event.preventDefault();
+            var target = this.hash
             $('body, html').animate({
-                scrollTop: $(this.hash).offset().top - $('#navbarFixed').height()
-            }, 800);
+                scrollTop: $(target).offset().top - $('#navbarFixed').height()
+            }, 1000, 'swing', function () {
+                window.location.hash = target
+            });
         }
     });
 })
