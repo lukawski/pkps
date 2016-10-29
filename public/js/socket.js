9b7b84236b090c0f18e6791090a9f936f7107f2a
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-10-29 14:17:39 +0200

    cleaned up

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-    var socket = io();
+/* global $, io, Cookies */
 
+$(document).ready(function () {
+  var socket = io()
 
-    if(Cookies.get('viewed')) {
-        
-    } else {
-        Cookies.set('viewed', true, {expires: 0.1});
-        socket.emit('addView');
-    }
+  if (!Cookies.get('viewed')) {
+    Cookies.set('viewed', true, { expires: 0.1 })
+    socket.emit('addView')
+  }
 
-    socket.on('views', function(views) {
-        console.log('Views: ' + views);
-        $('#views').text(views);
-    });
-});
+  socket.on('views', function (views) {
+    console.log('Views: ' + views)
+    $('#views').text(views)
+  })
+})
