db07644bd92c665420a522e2487e5d7083755173
ka.lukawski <ka.lukawski@gmail.com>
2017-02-08 07:04:45 -0500

    Changed folders structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,23 @@ gulp.task('default', ['js', 'img', 'css'], function () {
 })
 
 gulp.task('css', function () {
-  return gulp.src('./src/css/*.css')
+  return gulp.src('./src/css/**/*.css')
     .pipe(concat('bundle.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist/css'))
 })
 
 gulp.task('js', function () {
-  return gulp.src('./src/js/*.js')
+  return gulp.src('./src/js/**/*.js')
     .pipe(concat('bundle.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist/js'))
 })
 
 gulp.task('img', function () {
   return gulp.src('./src/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist/img'))
 })
 
 gulp.task('watch', ['js', 'css'], function () {
